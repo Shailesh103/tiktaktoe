e4e9b389e4b2d01b34577a4c5d9bdf49560e1bd4
Shailesh Maddhesiya <83523110+Shailesh103@users.noreply.github.com>
2024-01-27 11:45:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TikTacToe from './Components/TikTacToe/TikTacToe';
+
+function App() {
+  return (
+    <div>
+      <TikTacToe/>
+    </div>
+  );
+}
+
+export default App;
